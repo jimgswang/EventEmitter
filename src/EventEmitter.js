21d5e36babe4b9725ca2c4ac6f31617b35bf1df6
Jim Wang <jim.gs.wang@gmail.com>
2014-04-28 05:07:49 -0700

    add .on functionality

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -10,8 +10,21 @@ function EventEmitter() {
     this._events = {};
 }
 
+/**
+ * Register an event that will trigger listener
+ * @param {Mixed} eventName - The event to listen for
+ * @param {function} listener - The function to call when eventName happens
+ * @returns {Object} - Return the emitter
+ */
 EventEmitter.prototype.on = function(eventName, listener) {
 
+    if(typeof listener !== 'function') {
+        throw new TypeError('listener should be a function');
+    }
+
+    this._events[eventName] = this._events[eventName] || [];
+    this._events[eventName].push(listener);
+    return this;
 }
 
 module.exports = EventEmitter;
--- test/EventEmitter.test.js
+++ test/EventEmitter.test.js
@@ -13,13 +13,31 @@ describe('EventEmitter tests', function() {
     describe('.on', function() {
 
         it('should throw error if listener is not a function', function() {
-            expect(emitter.on('abc', 'abc')).to.throw(TypeError);
+            var fn = emitter.on.bind(null, 'abc', 'abc');
+            expect(fn).to.throw(TypeError);
         });
 
         it('should register event with emitter._events', function() {
             var listener = sinon.spy();
             emitter.on('data', listener);
-            expect(emitter._events.data[0] === listener);
+            expect(emitter._events.data[0]).to.equal(listener);
+        });
+
+        it('should be able to register multiple listeners', function() {
+            var listener1 = sinon.spy();
+            var listener2 = sinon.spy();
+            emitter.on('data', listener1);
+            emitter.on('data', listener2);
+
+            expect(emitter._events.data[0]).to.equal(listener1);
+            expect(emitter._events.data[1]).to.equal(listener2);
+        });
+
+        it('should return itself', function() {
+
+            var listener = sinon.spy();
+            expect(emitter.on('data', listener)).to.equal(emitter);
+
         });
     });
 
